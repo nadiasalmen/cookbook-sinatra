443edee1cab9ca1593e3e2f6fa772521cc6702b7
Nadia Salmen <nadiasalmen@gmail.com>
2020-11-07 20:29:05 -0300

    Add recipe and cookbook files

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require 'better_errors'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/about' do
+  erb :about
 end
